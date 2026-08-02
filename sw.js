83d48db24a604ea2b1b96cbab3fd2916e65ce747
welcome-to-batam <cowokperenung@gmail.com>
2026-08-02 16:52:09 +0700

    Update cache version to v131

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alfa-cache-v130'; // Naikkan versi ke v130
+const CACHE_NAME = 'alfa-cache-v131'; // Naikkan versi ke v131
 const assets = [
   '/ALFA-Online-Shopping/',
   '/ALFA-Online-Shopping/index.html',
